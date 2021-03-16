85c6ee7de05f84b4ddb8ddb8a919953fd02f5668
Alim Mamutov <mamutov.creation@yandex.ru>
2021-03-16 16:21:54 +0300

    My homework 1

--- Ex_1.py
+++ Ex_1.py
@@ -1,5 +1,5 @@
-#Lesson1
-durations = {'min': 60,'hour': 3600,'day': 86400,'month': 2678400,'year': 977616000}
+# Lesson1
+durations = {'min': 60, 'hour': 3600, 'day': 86400, 'month': 2678400, 'year': 977616000}
 
 user_sec = int(input('Введите количество секунд: '))
 sec_count = user_sec
@@ -16,8 +16,13 @@ sec_count = sec_count - days*durations['day']
 hours = sec_count // durations['hour']
 sec_count = sec_count - hours*durations['hour']
 
-mins = sec_count // durations['min']
-sec_count = sec_count - mins*durations['min']
+minuts = sec_count // durations['min']
+sec_count = sec_count - minuts * durations['min']
 
-format_str = f'{user_sec} секунд = {years} лет {months} месяцев {days} дней {hours} часов {mins} минут {sec_count} секунд'
+format_str = f'{user_sec} секунд = {years} лет ' \
+             f'{months} месяцев ' \
+             f'{days} дней ' \
+             f'{hours} часов ' \
+             f'{minuts} минут ' \
+             f'{sec_count} секунд '
 print(format_str)
